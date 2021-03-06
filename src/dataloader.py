06db62371515a8481d342aa7c84d6c116cea53da
Stephen Ren <stephrenny23@gmail.com>
2021-03-06 14:37:26 -0800

    Incorporated stylegan implementation

--- src/dataloader.py
+++ src/dataloader.py
@@ -10,13 +10,24 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from torch.utils.data.sampler import Sampler
 
+"""
+Dataset for loading in alphanumeric (abc..789) character images of given fonts
+"""
 class FontDataset(Dataset):
-    def __init__(self, fonts_directory, regular_only=False):
+    """
+    @ params
+    fonts_directory (str): High level directory which contains subdirectories of fonts
+        The subdirectories are named by their font name (e.g. Helvetica-Regular)
+        Each directory contains images of lower and uppercase characters and numerals 0-9
+        Images of 
+    num_condition (int, tuple<int>): The number of characters to stack together as a condition
+        If tuple, gives a low-high range for number of characters given
+    """
+    def __init__(self, fonts_directory, num_condition, regular_only=False):
         self.fonts_directory = fonts_directory
+        self.num_condition = num_condition
 
-        self.length = 0
-        for font_name in os.listdir(fonts_directory):
-            self.length += 1
+        self.process_fonts(self.fonts_directory)
 
     def _process_fonts(self):
         self.fonts = []
@@ -27,6 +38,14 @@ class FontDataset(Dataset):
     def __len__(self):
         return self.length
 
-    # Shouldn't __getitem__ be giving back entire lists of characters at a time?
+    """
+    Returns an font item of a certain font type (e.g. Helvetica-Reular)
+    In the form of a condition, target tuple:
+    condition: (n, w, h) tensor of n random character images
+    target: (62, w, h) tensor of all the alphanumeric character images
+    """
     def __getitem__(self, idx):
         pass
+
+class CharacterDataset(Dataset):
+    pass
