c28da41a8d98eff22adf9e94ad402bafc88340dc
Stephen Ren <stephrenny23@gmail.com>
2021-03-09 12:26:41 -0800

    Set up training to allow for false-positives to the discriminator

--- src/dataloader.py
+++ src/dataloader.py
@@ -16,6 +16,8 @@ from torch.utils.data.sampler import Sampler
 """
 Dataset for loading in alphanumeric (abc..789) character images of given fonts
 """
+
+
 class FontDataset(Dataset):
     """
     @ params
@@ -26,13 +28,16 @@ class FontDataset(Dataset):
     num_condition (int, tuple<int>): The number of characters to stack together as a condition
         If tuple, gives a low-high range for number of characters given
     """
-    def __init__(self, fonts_directory, dims=(256,256), max_img_dim=128, regular_only=False, src_corpus=None, tgt_corpus=None):
+
+    def __init__(self, fonts_directory, dims=(256, 256), max_img_dim=128, regular_only=False, src_corpus=None, tgt_corpus=None):
         self.fonts_directory = fonts_directory
         self.dims = dims
-        self.max_img_dim=128
+        self.max_img_dim = 128
         self.regular_only = regular_only
-        self.src_corpus = src_corpus if src_corpus is not None else [['H', 'e-1', 'l-1', 'l-1', 'o-1']]
-        self.tgt_corpus = tgt_corpus if tgt_corpus is not None else [['T', 'h-1', 'e-1', 'r-1', 'e-1']]
+        self.src_corpus = src_corpus if src_corpus is not None else [
+            ['H', 'e-1', 'l-1', 'l-1', 'o-1']]
+        self.tgt_corpus = tgt_corpus if tgt_corpus is not None else [
+            ['T', 'h-1', 'e-1', 'r-1', 'e-1']]
 
         self.fonts = []
         for font_name in os.listdir(self.fonts_directory):
@@ -51,21 +56,23 @@ class FontDataset(Dataset):
             if filename[0] == '.':
                 continue
             max_h, max_w = self.max_img_dim, self.max_img_dim
-            image = cv2.imread(os.path.join(char_dir, filename), cv2.IMREAD_GRAYSCALE).astype(np.float) / 255
+            image = cv2.imread(os.path.join(char_dir, filename),
+                               cv2.IMREAD_GRAYSCALE).astype(np.float) / 255
             h, w = image.shape
             image = image[:min(self.max_img_dim, h), :min(self.max_img_dim, w)]
-            h, w = image.shape
-            pad_h, pad_w = self.max_img_dim, self.max_img_dim
-            
-            padded_image = np.ones((self.max_img_dim, self.max_img_dim)).astype(np.float)
-            h_start, w_start = (pad_h - h) // 2, (pad_w - w) // 2
-            padded_image[h_start:h_start + h, w_start: w_start + w] = image
+#             h, w = image.shape
+#             pad_h, pad_w = min(self.max_img_dim, h), min(self.max_img_dim, w)
 
-            # Key = 'a', 'b', 'Z', '0', etc
-            out[str(filename.split('.')[0])] = padded_image
-        
+#             padded_image = np.ones(
+#                 (pad_h, pad_w)).astype(np.float)
+#             h_start, w_start = (pad_h - h) // 2, (pad_w - w) // 2
+#             padded_image[h_start:h_start + h, w_start: w_start + w] = image
+
+#             # Key = 'a', 'b', 'Z', '0', etc
+            out[str(filename.split('.')[0])] = image
+
         return out
-    
+
     def _shape_image(self, image, cfg=None):
         out_h, out_w = self.dims
         if cfg is None:
@@ -73,17 +80,21 @@ class FontDataset(Dataset):
             span = np.random.uniform(0.5, 1)
             scale = out_w * span / w
             new_h, new_w = int(h * scale), int(w * scale)
-            start_h, start_w = np.random.randint(0, out_h - new_h), np.random.randint(out_w - new_w)
+            start_h, start_w = np.random.randint(
+                0, out_h - new_h), np.random.randint(out_w - new_w)
             cfg = start_h, start_w, scale
-        
+
         h, w = image.shape
         start_h, start_w, scale = cfg
+        # Scales for different fonts may get messed up
+        if w * scale + start_w > out_w:
+            scale = (out_w - start_w) / w
         new_h, new_w = int(h * scale), int(w * scale)
         out = np.ones((out_h, out_w)).astype(np.float)
         img = cv2.resize(image, (new_w, new_h))
         out[start_h: start_h + new_h, start_w: start_w + new_w] = img
         return out, cfg
-        
+
         # if w <= self.dims[1]:
         #     w_start = (self.dims[1] - w) // 2
         #     out[:, w_start:w_start + w] = image
@@ -92,47 +103,41 @@ class FontDataset(Dataset):
         #     img = cv2.resize(image, (self.dims[1], new_h))
         #     h_start = (self.dims[0] - new_h) // 2
         #     out[h_start: h_start + new_h, :] = img
-        
+
         # return out
 
     """
-    Returns an font item of a certain font type (e.g. Helvetica-Reular)
-    In the form of a condition, target tuple:
-    condition: (w, h, 62) tensor of n random character images, where most of the 62 is noise
-    target: (w, h, 62) tensor of all the alphanumeric character images
+    Returns an font image of a certain type (i.e. Helevtica Regular)
     """
+
     def __getitem__(self, idx):
         char_images = self._load_char_images(idx)
-        rnd_char_images = self._load_char_images(np.random.randint(self.__len__()))
-        
+        rnd_char_images = self._load_char_images(np.random.choice(
+            [x for x in range(self.__len__()) if x != idx]))
+        false_rnd_char_images = self._load_char_images(np.random.choice(
+            [x for x in range(self.__len__()) if x != idx]))
+
+        # TODO Change word and target word to randos
         word = ['H', 'e-1', 'l-1', 'l-1', 'o-1']
         target_word = ['T', 'h-1', 'e-1', 'r-1', 'e-1']
-        orig_font, cfg = self._shape_image(np.concatenate([char_images[key] for key in word], axis=1))
-        condition, _ = self._shape_image(np.concatenate([rnd_char_images[key] for key in target_word], axis=1))
-        target, _ = self._shape_image(np.concatenate([char_images[key] for key in target_word], axis=1), cfg)
+        orig_font, cfg = self._shape_image(np.concatenate(
+            [char_images[key] for key in word], axis=1))
+        condition, _ = self._shape_image(np.concatenate(
+            [rnd_char_images[key] for key in target_word], axis=1))
+        target, _ = self._shape_image(np.concatenate(
+            [char_images[key] for key in target_word], axis=1), cfg)
+        false_target, _ = self._shape_image(np.concatenate(
+            [false_rnd_char_images[key] for key in target_word], axis=1), cfg)
 
         orig_font = torch.from_numpy(1 - orig_font).unsqueeze(dim=0).float()
         condition = torch.from_numpy(1 - condition).unsqueeze(dim=0).float()
         target = torch.from_numpy(1 - target).unsqueeze(dim=0).float()
-        
-        return orig_font, condition, target
-        
-        # Sort the images by keys, turn into array
-        # char_images = 1 - np.array([char_images[key] for key in sorted(list(char_images.keys()))]).astype(np.float) / 255
+        false_target = torch.from_numpy(
+            1 - false_target).unsqueeze(dim=0).float()
 
-        # target = char_images
-        # condition = np.zeros((self.n_chars, self.dims[0], self.dims[1])).astype(np.float)
-        # # random indices of 
-        # condition_idxs = np.random.choice(self.n_chars, self.num_condition)
-        # condition[condition_idxs] = target[condition_idxs]
-        
-        # condition = condition.transpose(1, 2, 0)
-        # target = target.transpose(1, 2, 0)
-        
-        # condition = torch.from_numpy(condition)
-        # target = torch.from_numpy(target)
-        
-        # return condition, target, condition_idxs
+        # TODO generate 'fake' fonts
+
+        return orig_font, condition, target, false_target
 
     def __len__(self):
-        return len(self.fonts)        
+        return len(self.fonts)
--- train.py
+++ train.py
@@ -19,18 +19,27 @@ from tqdm import tqdm
 from src.models.fontGAN import FontGenerator, FontDiscriminator
 from src.dataloader import FontDataset
 
+"""
+Much of the training harness is recycled from the cs244n squad program
+"""
+
+
 def load(model, cpk_file):
     pretrained_dict = torch.load(cpk_file)
     model_dict = model.state_dict()
-    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+    pretrained_dict = {k: v for k,
+                       v in pretrained_dict.items() if k in model_dict}
     model_dict.update(pretrained_dict)
     model.load_state_dict(model_dict)
+
 
 def main(args=None):
     parser = argparse.ArgumentParser(description="FontGAN pytorch.")
 
-    parser.add_argument('--train_dir', type=str, default='data/font_characters/train')
-    parser.add_argument('--dev_dir', type=str, default='data/font_characters/dev')
+    parser.add_argument('--train_dir', type=str,
+                        default='data/font_characters/train')
+    parser.add_argument('--dev_dir', type=str,
+                        default='data/font_characters/dev')
     parser.add_argument('--save_dir', type=str, default='save')
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--seed', type=int, default=236)
@@ -44,7 +53,8 @@ def main(args=None):
 
     log = util.get_logger(args.save_dir, args.name)
 
-    tbx = SummaryWriter(util.get_save_dir(args.save_dir, args.name, training=True))
+    tbx = SummaryWriter(util.get_save_dir(
+        args.save_dir, args.name, training=True))
     device, args.gpu_ids = util.get_available_devices()
     args.batch_size *= max(1, len(args.gpu_ids))
 
@@ -66,21 +76,22 @@ def main(args=None):
     discriminator.train()
 
     # Set criterion
-    criterion = nn.BCEWithLogitsLoss()
+    criterion = F.binary_cross_entropy_with_logits
 
     # Get optimizer and scheduler
     gen_optimizer = optim.Adadelta(generator.parameters(), args.lr,
-                               weight_decay=args.l2_wd)
+                                   weight_decay=args.l2_wd)
     disc_optimizer = optim.Adadelta(discriminator.parameters(), args.lr,
-                               weight_decay=args.l2_wd)
+                                    weight_decay=args.l2_wd)
     gen_scheduler = sched.LambdaLR(gen_optimizer, lambda s: 1.)
     disc_scheduler = sched.LambdaLR(disc_optimizer, lambda s: 1.)
 
     # Build dataset
-    train_dataset = FontDataset(args.train_dir) # Path
-    dev_dataset = FontDataset(args.dev_dir) # Path
+    train_dataset = FontDataset(args.train_dir)  # Path
+    dev_dataset = FontDataset(args.dev_dir)  # Path
 
-    train_loader = data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    train_loader = data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True)
     dev_loader = data.DataLoader(dev_dataset, batch_size=args.batch_size)
 
     # Set up outputs folder
@@ -98,27 +109,39 @@ def main(args=None):
 
         epoch += 1
         with torch.enable_grad(), tqdm(total=len(train_loader.dataset)) as progress_bar:
-            for orig, condition, target in train_loader:
+            for orig, condition, target, false_target in train_loader:
                 curr_batch_size = len(orig)
                 progress_bar.update(curr_batch_size)
-                x = torch.cat([orig, condition], dim=1).to(device) # Pass into generator: original font + target text
-                output_real = torch.cat([target, orig], dim=1).to(device) # To pass into discriminator: target + original font
+                # Pass into generator: original font + target text
+                x = torch.cat([orig, condition], dim=1).to(device)
+                # To pass into discriminator: target + original font
+                output_real = torch.cat([target, orig], dim=1).to(device)
+
+                # Dataset-generated false fonts
+                # To pass into discriminator: false_target + original font
+                output_false = torch.cat(
+                    [false_target, orig], dim=1).to(device)
+
                 orig = orig.to(device)
 
                 # Update discriminator
                 disc_optimizer.zero_grad()
-                gen_imgs = generator(x)
-                output_fake = torch.cat([gen_imgs, orig], dim=1) # To pass into discriminator: generated + original font
+                gen_imgs = generator(x).detach()
+                # To pass into discriminator: generated + original font
+                output_fake = torch.cat([gen_imgs, orig], dim=1)
 
-                disc_pred = discriminator(torch.cat([output_real, output_fake], dim=0)).squeeze(dim=1) # shape = (batch_size,)
-                disc_real = disc_pred[:curr_batch_size] # First half is of the real images
-                disc_fake = disc_pred[curr_batch_size:] # Second half is of the fake images
+                disc_pred = discriminator(torch.cat([output_real, output_fake, output_false], dim=0)).squeeze(
+                    dim=1)  # shape = (batch_size,)
+                # First [batch_size] images are of the real
+                disc_real = disc_pred[:curr_batch_size]
+                # Rest are of fake images
+                disc_fake = disc_pred[curr_batch_size:]
 
                 labels_real = torch.ones_like(disc_real)
                 labels_fake = torch.zeros_like(disc_fake)
 
-                disc_real_loss = criterion(disc_real, labels_real) # BCE Loss
-                disc_fake_loss = criterion(disc_fake, labels_fake) # BCE Loss
+                disc_real_loss = criterion(disc_real, labels_real)  # BCE Loss
+                disc_fake_loss = criterion(disc_fake, labels_fake)  # BCE Loss
 
                 disc_loss = (disc_real_loss + disc_fake_loss) / 2
 
@@ -129,9 +152,11 @@ def main(args=None):
                 # Train generator
                 gen_optimizer.zero_grad()
                 gen_imgs = generator(x)
-                output_fake = torch.cat([gen_imgs, orig], dim=1) # To pass into discriminator: generated + original font
+                # To pass into discriminator: generated + original font
+                output_fake = torch.cat([gen_imgs, orig], dim=1)
                 disc_pred = discriminator(output_fake).squeeze(dim=1)
 
+                # We wish for the discrminator to mark these images as real
                 labels_fake = torch.ones_like(disc_fake)
 
                 gen_loss = criterion(disc_pred, labels_fake)
@@ -139,13 +164,14 @@ def main(args=None):
                 gen_optimizer.step()
 
                 progress_bar.set_postfix(epoch=epoch, GenLoss=gen_loss.item())
-                tbx.add_scalar('train/GenLoss', gen_loss.item(), step)                    
+                tbx.add_scalar('train/GenLoss', gen_loss.item(), step)
 
                 step += curr_batch_size
 
                 steps_til_eval -= curr_batch_size
                 if steps_til_eval <= 0:
-                    save_outputs(gen_imgs, orig, condition, target, step, 'outputs/{}/train'.format(args.name))
+                    save_outputs(gen_imgs, orig, condition, target,
+                                 step, 'outputs/{}/train'.format(args.name))
                     steps_til_eval = args.eval_steps
 
         # Save generator outputs and evaluate
@@ -154,6 +180,7 @@ def main(args=None):
         epoch += 1
 
     return 0
+
 
 def save_outputs(gen_images, orig, condition, target, step, save_dir):
     if not os.path.isdir(save_dir):
@@ -169,11 +196,16 @@ def save_outputs(gen_images, orig, condition, target, step, save_dir):
 
     idx = 0
     for gen_img, orig_img, target_img, condition_img in zip(gen_imgs, orig_imgs, target_imgs, condition_imgs):
-        cv2.imwrite(os.path.join(save_dir, 'step-{}'.format(step), 'orig-' + str(idx) + '.jpg'), orig_img)
-        cv2.imwrite(os.path.join(save_dir, 'step-{}'.format(step), 'gen-' + str(idx) + '.jpg'), gen_img)
-        cv2.imwrite(os.path.join(save_dir, 'step-{}'.format(step), 'target-' + str(idx) + '.jpg'), target_img)
-        cv2.imwrite(os.path.join(save_dir, 'step-{}'.format(step), 'condition-' + str(idx) + '.jpg'), condition_img)
+        cv2.imwrite(os.path.join(save_dir, 'step-{}'.format(step),
+                                 'orig-' + str(idx) + '.jpg'), orig_img)
+        cv2.imwrite(os.path.join(save_dir, 'step-{}'.format(step),
+                                 'gen-' + str(idx) + '.jpg'), gen_img)
+        cv2.imwrite(os.path.join(save_dir, 'step-{}'.format(step),
+                                 'target-' + str(idx) + '.jpg'), target_img)
+        cv2.imwrite(os.path.join(save_dir, 'step-{}'.format(step),
+                                 'condition-' + str(idx) + '.jpg'), condition_img)
         idx += 1
+
 
 def evaluate(gen, dataloader, device, epoch, step, tb, name):
     gen.eval()
@@ -190,16 +222,17 @@ def evaluate(gen, dataloader, device, epoch, step, tb, name):
 
             x = torch.cat([orig, condition], dim=1).to(device)
             gen_imgs = gen(x)
-            total_loss += float((gen_imgs - target).abs().mean(dim=(1,2,3)).sum())
+            total_loss += float((gen_imgs -
+                                 target).abs().mean(dim=(1, 2, 3)).sum())
 
             if not saved:
-                save_outputs(gen_imgs, orig, condition, target, step, 'outputs/{}/dev'.format(name))
+                save_outputs(gen_imgs, orig, condition, target,
+                             step, 'outputs/{}/dev'.format(name))
                 saved = True
 
     tb.add_scalar('dev/L1Loss', total_loss / len(dataloader), epoch)
     print('L1: {}'.format(total_loss / len(dataloader)))
     gen.train()
-
 
 
 if __name__ == '__main__':
